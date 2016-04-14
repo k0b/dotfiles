0dd986ec8e7de981d9e5310ecb00c99895353536
James Spurin <james@spurin.com>
2016-04-14 22:02:48 +0100

    cleaning up unnecessary files

--- .atom/compile-cache/coffee/da39a3ee5e6b4b0d3255bfef95601890afd80709.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL2phbWVzLy5hdG9tL3BhY2thZ2VzL3NldGktaWNvbnMvZmlsZS10eXBlcy9Db2ZmZVNjcmlwdC5jb2ZmZWUiCiAgXSwKICAibmFtZXMiOiBbXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUFBO0FBQUEiCn0=
-
-//# sourceURL=/Users/james/.atom/packages/seti-icons/file-types/CoffeScript.coffee
